587e967064b84762c6554f4ae8ee9a331fb83a4e
francescomani <email@francescomani.it>
2026-04-02 10:38:29 +0200

    check for PTRS in function to get RE for PUSCH
    Signed-off-by: Francesco Mani <email@francescomani.it>

--- openair1/PHY/NR_TRANSPORT/nr_ulsch_demodulation.c
+++ openair1/PHY/NR_TRANSPORT/nr_ulsch_demodulation.c
@@ -244,18 +244,24 @@ static void nr_ulsch_extract_rbs(c16_t *const rxF,
   }
 }
 
-static int get_nb_re_pusch (NR_DL_FRAME_PARMS *frame_parms, const nfapi_nr_pusch_pdu_t *rel15_ul, int symbol)
+static int get_nb_re_pusch(NR_DL_FRAME_PARMS *frame_parms,
+                           const nfapi_nr_pusch_pdu_t *rel15_ul,
+                           int symbol,
+                           int ptrs_symb_pos,
+                           int ptrs_re_symbol)
 {
-  uint8_t dmrs_symbol_flag = (rel15_ul->ul_dmrs_symb_pos >> symbol) & 0x01;
-  if (dmrs_symbol_flag == 1) {
+  int re_pusch = rel15_ul->rb_size * NR_NB_SC_PER_RB;
+  if ((rel15_ul->ul_dmrs_symb_pos >> symbol) & 0x01) {
     if (rel15_ul->dmrs_config_type == 0) {
       // if no data in dmrs cdm group is 1 only even REs have no data
       // if no data in dmrs cdm group is 2 both odd and even REs have no data
-      return(rel15_ul->rb_size *(12 - (rel15_ul->num_dmrs_cdm_grps_no_data*6)));
-    }
-    else return(rel15_ul->rb_size *(12 - (rel15_ul->num_dmrs_cdm_grps_no_data*4)));
-  } else
-    return (rel15_ul->rb_size * NR_NB_SC_PER_RB);
+      re_pusch -= rel15_ul->rb_size * rel15_ul->num_dmrs_cdm_grps_no_data * 6;
+    } else
+      re_pusch -= rel15_ul->rb_size * rel15_ul->num_dmrs_cdm_grps_no_data * 4;
+  }
+  if (IS_BIT_SET(ptrs_symb_pos, symbol))
+    re_pusch -= ptrs_re_symbol;
+  return re_pusch;
 }
 
 static void inner_rx(PHY_VARS_gNB *gNB,
@@ -873,12 +879,17 @@ int nr_rx_pusch_group_tp(PHY_VARS_gNB *gNB,
     nr_codeword_unscrambling_init(scrambling_sequences_arr[u], G[u], 0, p->data_scrambling_id, p->rnti);
   }
 
-  // Computation of channel levels
-  int nb_re_pusch = 0, meas_symbol = -1;
-  for (meas_symbol = rel15_ul_ref->start_symbol_index; meas_symbol < end_symbol; meas_symbol++)
-    if ((nb_re_pusch = get_nb_re_pusch(frame_parms, &joint_pdu, meas_symbol)) > 0)
-      break;
+  int meas_symbol = -1;
+  for (int sym = 0; sym < frame_parms->symbols_per_slot; sym++) {
+    if (sym >= rel15_ul_ref->start_symbol_index && sym < rel15_ul_ref->start_symbol_index + rel15_ul_ref->nr_of_symbols) {
+      joint_pv->ul_valid_re_per_slot[sym] = get_nb_re_pusch(frame_parms, &joint_pdu, sym, ptrs_info.ptrs_symbols, ptrs_re_symbol);
+      if (meas_symbol == -1 && joint_pv->ul_valid_re_per_slot[sym] != 0)
+        meas_symbol = sym;
+    } else
+      joint_pv->ul_valid_re_per_slot[sym] = 0;
+  }
 
+  int nb_re_pusch = joint_pv->ul_valid_re_per_slot[meas_symbol];
   AssertFatal(nb_re_pusch > 0 && meas_symbol >= 0,
               "nb_re_pusch %d cannot be 0 or meas_symbol %d cannot be negative here\n",
               nb_re_pusch,
@@ -959,8 +970,6 @@ int nr_rx_pusch_group_tp(PHY_VARS_gNB *gNB,
     int res_per_task = 0;
     for (int s = 0; s < numSymbols && s + symbol < end_symbol; s++) {
       int curr_sym = symbol + s;
-      joint_pv->ul_valid_re_per_slot[curr_sym] =
-          get_nb_re_pusch(frame_parms, &joint_pdu, curr_sym) - (IS_BIT_SET(ptrs_info.ptrs_symbols, (symbol + s)) ? ptrs_re_symbol : 0);
       if (curr_sym == rel15_ul_ref->start_symbol_index) {
         joint_pv->llr_offset[curr_sym] = 0;
       } else {
