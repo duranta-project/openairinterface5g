c6e7dd1e1ee71b31a57de0d5dcc963c493f0cfe9
Guido Casati <guido.casati@openairinterface.org>
2026-05-19 23:57:18 +0200

    nr-up: add static lib for the PDCP boundary and link L2_NR and nr-cuup
    Introduce the nr_up_pdcp_if static library and link it into L2_NR
    (monolithic gNB via nr-softmodem) and nr-cuup. Add empty nr_up.h for
    shared wire types. This is a placeholder lib, the first step before
    APIs implementation for the PDCP boundary (gNB/CU side).
    
    Also, registers the NR_UP log component and legacy T-tracer message IDs.
    
    Changes:
    - openair2/LAYER2/nr_up/: add nr_up CMake target
                              add empty nr_up_pdcp_if.c/h, nr_up.h
    - CMakeLists.txt: link nr_up_pdcp_if into L2_NR and nr-cuup
    - log.h and T_messages.txt: NR_UP logging component
    
    Signed-off-by: Guido Casati <guido.casati@openairinterface.org>

---
+++ openair2/LAYER2/nr_up/nr_up_pdcp_if.c
@@ -0,0 +1,5 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
+
+#include "nr_up/nr_up_pdcp_if.h"
---
+++ openair2/LAYER2/nr_up/nr_up_pdcp_if.h
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
+
+#ifndef NR_UP_PDCP_IF_H
+#define NR_UP_PDCP_IF_H
+
+#endif /* NR_UP_PDCP_IF_H */
