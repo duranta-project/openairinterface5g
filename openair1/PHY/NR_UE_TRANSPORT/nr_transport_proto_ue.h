67ed6b48e6eedec1501db70d320044247ef33da7
Robert Schmidt <robert.schmidt@openairinterface.org>
2026-05-19 08:56:34 +0200

    PSCCH

--- openair1/PHY/NR_UE_TRANSPORT/nr_transport_proto_ue.h
+++ openair1/PHY/NR_UE_TRANSPORT/nr_transport_proto_ue.h
@@ -362,6 +362,13 @@ uint32_t nr_generate_sci(PHY_VARS_NR_UE *ue,
                          NR_DL_FRAME_PARMS *frame_parms,
                          int slot);
 
+uint32_t nr_generate_sci1(const PHY_VARS_NR_UE *ue,
+                          c16_t *txdataF,
+                          const NR_DL_FRAME_PARMS *frame_parms,
+                          const int16_t amp,
+                          const int nr_slot_tx,
+                          const sl_nr_tx_config_pscch_pssch_pdu_t *pscch_pssch_pdu);
+
 void nr_generate_psfch0(const PHY_VARS_NR_UE *ue,
                         c16_t **txdataF,
                         const NR_DL_FRAME_PARMS *frame_parms,
