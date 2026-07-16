dcf7f365ec1d0a023e985431b03fb92527172a9e
Laurent THOMAS <laurent.thomas@open-cells.com>
2026-07-16 14:58:13 +0200

    disable write reorder clear context for rfsim. The purpose of this is not very clear, for multi-ue on one ru, it is not good

--- executables/nr-ue-ru.c
+++ executables/nr-ue-ru.c
@@ -544,5 +544,6 @@ void nrue_ru_write_reorder_clear_context(PHY_VARS_NR_UE *UE)
 {
   openair0_device_t *device = &nrue_rus.openair0_dev[UE->rf_map.card];
   LOG_W(HW, "[UE %d] received write reorder clear context\n", UE->Mod_id);
-  openair0_write_reorder_clear_context(device);
+  if (!IS_SOFTMODEM_RFSIM)
+     openair0_write_reorder_clear_context(device);
 }
