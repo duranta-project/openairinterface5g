0e9fcacd6515e161a4868dd9b372302ad8bb5274
Karim Boutiba <karim.boutiba@openairinterface.org>
2026-07-29 09:26:31 +0000

    Replace SR boolean by sr_cnt Replace the SR boolean with sr_cnt to track the number of received SRs before scheduling a UL grant. When connecting more than 64 UEs, UEs with pending SRs were not scheduled before reaching the configured maximum number of SR retransmissions, causing them to be dropped. Add a warning to detect this case.
    Signed-off-by: Karim Boutiba <karim.boutiba@openairinterface.org>

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_uci.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_uci.c
@@ -986,7 +986,13 @@ void handle_nr_uci_pucch_0_1(module_id_t mod_id, int cell_id, frame_t frame, slo
   if (uci_01->pduBitmap & 0x1) {
     if (uci_01->sr.sr_indication && uci_01->sr.sr_confidence_level == 0 && uci_01->ul_cqi >= 148) {
       // SR detected with SNR >= 10dB
-      sched_ctrl->SR |= true;
+      sched_ctrl->sr_cnt++;
+      if (sched_ctrl->sr_cnt >= nrmac->cells[cell_id].radio_config.timer_config.sr_TransMax)
+        LOG_W(NR_MAC,
+              "UE %04x: %d SR received, reached configured sr_TransMax %d before UL grant\n",
+              uci_01->rnti,
+              sched_ctrl->sr_cnt,
+              nrmac->cells[cell_id].radio_config.timer_config.sr_TransMax);
       LOG_D(NR_MAC, "SR UE %04x ul_cqi %d\n", uci_01->rnti, uci_01->ul_cqi);
     }
 
@@ -1026,7 +1032,7 @@ void handle_nr_uci_pucch_2_3_4(module_id_t mod_id, int cell_id, frame_t frame, s
 
   if (uci_234->pduBitmap & 0x1) {
     if (uci_234->sr.sr_payload && uci_234->sr.sr_payload[0])
-      sched_ctrl->SR = true;
+      sched_ctrl->sr_cnt++;
     free(uci_234->sr.sr_payload);
   }
 
