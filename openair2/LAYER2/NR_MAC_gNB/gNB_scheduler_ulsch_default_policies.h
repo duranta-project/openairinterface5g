0388b6a9d04246710993debcdcd95c0f3a91417e
Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>
2026-07-29 10:10:28 +0530

    Remove static in nr_ul_port_select_default() to use in MU MIMO scheduler
    Signed-off-by: Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch_default_policies.h
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch_default_policies.h
@@ -31,4 +31,6 @@ void nr_ul_mcs_select_default(const nr_cell_sched_t *cell, nr_ul_candidate_t *ca
 
 int nr_ul_proportional_fair(const nr_ul_sched_params_t *params, nr_ul_candidate_t *candidates, int n_candidates);
 
+void nr_ul_port_select_default(const nr_ul_sched_params_t *params, nr_ul_candidate_t *cand);
+
 #endif /* GNB_SCHEDULER_ULSCH_DEFAULT_POLICIES_H */
