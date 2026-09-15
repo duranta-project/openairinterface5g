85cdec53ff069af8bb2e63daa654cf5e8b98056b
Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>
2026-09-15 20:14:08 +0530

    Add a log to compare I0 noise measurements and SRS noise
    Signed-off-by: Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>

--- openair1/SCHED_NR/phy_procedures_nr_gNB.c
+++ openair1/SCHED_NR/phy_procedures_nr_gNB.c
@@ -799,6 +799,21 @@ nr_srs_info_t nr_srs_rx_procedures(PHY_VARS_gNB *gNB,
     noise_power_avg /= nb_antennas_rx;
     *snr = dB_fixed(signal_power_avg) - dB_fixed(max(noise_power_avg, 1));
 
+    const int srs_noise_dB = dB_fixed(max(noise_power_avg, 1));
+    const int n0_noise_per_ant_dB = gNB->measurements.n0_subband_power_avg_dB - dB_fixed(frame_parms->nb_antennas_rx);
+    LOG_I(NR_PHY,
+          "%d.%d RNTI %04x: noise per antenna: SRS %d dB (lin %u) vs I0 %d dB (n0_subband_power_avg_dB %d over %d ant), "
+          "diff %d dB\n",
+          frame_rx,
+          slot_rx,
+          srs_pdu->rnti,
+          srs_noise_dB,
+          noise_power_avg,
+          n0_noise_per_ant_dB,
+          gNB->measurements.n0_subband_power_avg_dB,
+          frame_parms->nb_antennas_rx,
+          srs_noise_dB - n0_noise_per_ant_dB);
+
     const uint16_t m_SRS_b = get_m_srs(srs_pdu->config_index, srs_pdu->bandwidth_index);
     for (int rb = 0; rb < m_SRS_b; rb++) {
       snr_per_rb[rb] = dB_fixed(signal_power_avg) - dB_fixed(max(noise_power_per_rb[rb] / nb_antennas_rx, 1));
