e02a00f505b0c2a622da7ba0a212f51c08a288dc
Tim O'Shea <tim@deepsig.ai>
2026-09-18 11:06:29 +0000

    ldpc: handle rate-matched blocks shorter than the filler offset
    Foffset greater than E is valid. Bound each transmit copy by the
    remaining output length and skip filler bits while wrapping the circular
    buffer. Validate filler geometry against Ncb instead of E in transmit
    and both scalar and SIMD 16-bit receive paths.
    
    Specification/API reference:
    3GPP TS 38.212 V17.2.0, clause 5.4.2.1 (circular-buffer bit
    selection and skipping NULL filler bits).
    https://www.etsi.org/deliver/etsi_ts/138200_138299/138212/17.02.00_60/ts_138212v170200p.pdf
    
    Signed-off-by: Tim O'Shea <tim@deepsig.ai>

--- openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_segment/nr_rate_matching.c
+++ openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_segment/nr_rate_matching.c
@@ -1059,63 +1059,33 @@ int nr_rate_matching_ldpc(uint32_t Tbslbrm,
          Tbslbrm);
 #endif
 
-  if (Foffset > E) {
-    LOG_E(PHY,
-          "nr_rate_matching: invalid parameters (Foffset %d > E %d) F %d, k0 %d, Ncb %d, rvidx %d, Tbslbrm %d\n",
-          Foffset,
-          E,
-          F,
-          ind,
-          Ncb,
-          rvidx,
-          Tbslbrm);
+  // TS 38.212, 5.4.2.1 selects E non-NULL bits; E need not reach the filler offset.
+  if (Foffset > Ncb || F > Ncb - Foffset || F == Ncb) {
+    LOG_E(PHY, "nr_rate_matching: invalid filler interval (offset %d, length %d, Ncb %d)\n", Foffset, F, Ncb);
     return -1;
   }
-  if (Foffset > Ncb) {
-    LOG_E(PHY, "nr_rate_matching: invalid parameters (Foffset %d > Ncb %d)\n", Foffset, Ncb);
-    return -1;
-  }
-
-  if (ind >= Foffset && ind < (F + Foffset))
-    ind = F + Foffset;
 
   uint32_t k = 0;
-  if (ind < Foffset) { // case where we have some bits before the filler and the rest after
-    memcpy((void *)e, (void *)(d + ind), Foffset - ind);
+  while (k < E) {
+    if (ind < Foffset) {
+      const uint32_t count = min(Foffset - ind, E - k);
+      memcpy(e + k, d + ind, count);
+      ind += count;
+      k += count;
+    }
 
-    if (E + F <= Ncb - ind) { // E+F doesn't contain all coded bits
-      memcpy((void *)(e + Foffset - ind), (void *)(d + Foffset + F), E - Foffset + ind);
-      k = E;
-    } else {
-      memcpy((void *)(e + Foffset - ind), (void *)(d + Foffset + F), Ncb - Foffset - F);
-      k = Ncb - F - ind;
-    }
-  } else {
-    if (E <= Ncb - ind) { // E+F doesn't contain all coded bits
-      memcpy((void *)(e), (void *)(d + ind), E);
-      k = E;
-    } else {
-      memcpy((void *)(e), (void *)(d + ind), Ncb - ind);
-      k = Ncb - ind;
-    }
-  }
+    if (ind >= Foffset && ind < Foffset + F)
+      ind = Foffset + F;
 
-  while (k < E) { // case where we do repetitions (low mcs)
-    // chunk before filler: d[0 .. Foffset)
-    if (Foffset > 0) {
-      uint32_t n = min(Foffset, E - k);
-      memcpy(e + k, d, n);
-      k += n;
-      if (k >= E)
-        break;
+    if (ind < Ncb) {
+      const uint32_t count = min(Ncb - ind, E - k);
+      memcpy(e + k, d + ind, count);
+      ind += count;
+      k += count;
     }
-    // chunk after filler: d[Foffset+F .. Ncb)
-    uint32_t after = Ncb - Foffset - F;
-    if (after > 0) {
-      uint32_t n = min(after, E - k);
-      memcpy(e + k, d + Foffset + F, n);
-      k += n;
-    }
+
+    if (ind == Ncb)
+      ind = 0;
   }
 
   return 0;
@@ -1151,12 +1121,8 @@ int nr_rate_matching_ldpc_rx_simd(uint32_t Tbslbrm,
   const uint32_t N = geo.N;
   const uint32_t Ncb = geo.Ncb;
   uint32_t ind = geo.k0;
-  if (Foffset > E) {
-    LOG_E(PHY, "nr_rate_matching: invalid parameters (Foffset %d > E %d)\n", Foffset, E);
-    return -1;
-  }
-  if (Foffset > Ncb) {
-    LOG_E(PHY, "nr_rate_matching: invalid parameters (Foffset %d > Ncb %d)\n", Foffset, Ncb);
+  if (Foffset > Ncb || F > Ncb - Foffset || F == Ncb) {
+    LOG_E(PHY, "nr_rate_matching: invalid filler interval (offset %d, length %d, Ncb %d)\n", Foffset, F, Ncb);
     return -1;
   }
 
@@ -1224,12 +1190,8 @@ int nr_rate_matching_ldpc_rx(uint32_t Tbslbrm,
   const uint32_t N = geo.N;
   const uint32_t Ncb = geo.Ncb;
   uint32_t ind = geo.k0;
-  if (Foffset > E) {
-    LOG_E(PHY, "nr_rate_matching: invalid parameters (Foffset %d > E %d)\n", Foffset, E);
-    return -1;
-  }
-  if (Foffset > Ncb) {
-    LOG_E(PHY, "nr_rate_matching: invalid parameters (Foffset %d > Ncb %d)\n", Foffset, Ncb);
+  if (Foffset > Ncb || F > Ncb - Foffset || F == Ncb) {
+    LOG_E(PHY, "nr_rate_matching: invalid filler interval (offset %d, length %d, Ncb %d)\n", Foffset, F, Ncb);
     return -1;
   }
 
